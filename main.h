d38317053e2ec6c848354f718a7e154cf365189d
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-17 11:01:01 +0300

    My commit

--- main.h
+++ main.h
@@ -9,4 +9,16 @@ int print_str(va_list ap);
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
+
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p
+
 #endif
