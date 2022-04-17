5ff679df75d1b0ca4f55bbff47e79c7a5e660cda
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-17 11:24:31 +0300

    My commit

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -40,4 +40,4 @@ int print_less(va_list less);
 int print_space(va_list space);
 int print_sharp(va_list sharp);
 
-#endif  /* _HOLBERTON_H */
+#endif  /* _MAIN_H */
