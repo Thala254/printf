e60499f1965a44ef79553704e2fd433d1603f846
Brendabett <brendabett33@gmail.com>
2022-04-17 00:47:40 -0700

    My commit

--- _printf.c
+++ _printf.c
@@ -1,26 +1,85 @@
-#include "main.h"
+#include <stdarg.h>
+#include holberton.h
+#include <stddef.h>
 
 /**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
  */
+
+int (*get_op(const char c))(va_list)
+{
+int i = 0;
+
+flags_p fp[] = {
+{c, print_char},
+{s, print_str},
+{i, print_nbr},
+{d, print_nbr},
+{b, print_binary},
+{o, print_octal},
+{x, print_hexa_lower},
+{X, print_hexa_upper},
+{u, print_unsigned},
+{S, print_str_unprintable},
+{r, print_str_reverse},
+{p, print_ptr},
+{R, print_rot13},
+{%, print_percent}
+};
+while (i < 14)
+{
+if (c == fp[i].c[0])
+{
+return (fp[i].f);
+}
+i++;
+}
+return (NULL);
+}
+
+/**
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
+ */
+
 int _printf(const char *format, ...)
 {
-buffer_t *output;
-va_list args;
-int ret;
+va_list ap;
+int sum = 0, i = 0;
+int (*func)();
 
-if (format == NULL)
 return (-1);
-output = init_buffer();
-if (output == NULL)
-return (-1);
+va_start(ap, format);
 
-va_start(args, format);
-
-ret = run_printf(format, args, output);
-
-return (ret);
+while (format[i])
+{
+if (format[i] == '%')
+{
+if (format[i + 1] != '\0')
+func = get_op(format[i + 1]);
+if (func == NULL)
+{
+_putchar(format[i]);
+sum++;
+i++;
+}
+else
+{
+sum += func(ap);
+i += 2;
+continue;
+}
+}
+else
+{
+_putchar(format[i]);
+sum++;
+i++;
+}
+}
+va_end(ap);
+return (sum);
 }
