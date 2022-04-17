ba5c5fb279b9769bf17f459e1dc13ea3232e84e6
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-17 10:55:27 +0300

    My commit

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -8,5 +7,6 @@ int print_percent(va_list ap __attribute__((unused)));
 int print_char(va_list ap);
 int print_str(va_list ap);
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif
