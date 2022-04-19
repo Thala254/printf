ce0adc1ac098bc49a23ea4742832037d0b77b06c
Brendabett <brendabett33@gmail.com>
2022-04-18 23:43:16 -0700

    My commit

---
+++ get_flags.c
@@ -0,0 +1,31 @@
+#include main.h
+
+int get_flags(char s, mods *f);
+
+/**
+*get_flags - turns on flags if _printf finds
+*a flag modifier in the format string
+*@s: character that holds the flag specifier
+*@f: pointer to the struct flags in which we turn the flag on
+*
+*Return: 1 if a flag has been turned on, 0 otherwise
+*/
+
+int get_flags(char s, mods *f)
+{
+_Bool modifier = true;
+switch (s)
+{
+case '+':
+f-> plus = true;
+break;
+case ' ':
+f-> space = true;
+break;
+case '#':
+f-> harsh = true;
+default:
+modifier = false;
+}
+return (modifier);
+}
