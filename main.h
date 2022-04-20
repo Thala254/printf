c66db669f2e60975738bd47b8275fce77d1ea57e
thala254 <thalamine@gmail.com>
2022-04-20 10:06:18 +0300

    Updated README.md

--- main.h
+++ main.h
@@ -13,8 +13,22 @@ typedef struct print
 	int (*f)(va_list);
 } print_t;
 
+/* struct modifiers - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct modifiers
+{
+ _Bool plus;
+ _Bool space;
+ _Bool hash;
+} mods;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int get_flags(char s, mods *f);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
